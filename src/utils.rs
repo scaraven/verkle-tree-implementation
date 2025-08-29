102f852d45bbce5fc49bdcf95d1af3ee9c724378
scaraven <sc4raven@gmail.com>
2025-08-29 14:45:11 +0100

    feat: add hash to field

--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,6 @@
 use ark_bls12_381::Fr;
 use ark_poly::{univariate::DensePolynomial, DenseUVPolynomial, EvaluationDomain, Radix2EvaluationDomain as Domain};
+use ark_ff::PrimeField;
 
 pub(crate) fn evals_to_poly(domain: &Domain<Fr>, evals: &[Fr]) -> DensePolynomial<Fr> {
     assert_eq!(domain.size(), evals.len());
@@ -7,3 +8,10 @@ pub(crate) fn evals_to_poly(domain: &Domain<Fr>, evals: &[Fr]) -> DensePolynomia
     let coeffs = domain.ifft(evals);
     DensePolynomial::from_coefficients_vec(coeffs)
 }
+
+pub(crate) fn hash_to_field(bytes: &[u8]) -> Fr {
+    let binding = blake3::hash(bytes);
+    let h = binding.as_bytes();  // 32 bytes
+    Fr::from_le_bytes_mod_order(h)
+}
+
